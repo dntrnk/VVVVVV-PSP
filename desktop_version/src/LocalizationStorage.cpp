656eb2681d5a15d8468597bbb1fba97a29d2a3a3
dntrnk <dntrnk.dev@gmail.com>
2026-09-17 21:44:49 +0700

    Disable big fonts loading for now

--- desktop_version/src/LocalizationStorage.cpp
+++ desktop_version/src/LocalizationStorage.cpp
@@ -1083,6 +1083,9 @@ void loadlanguagelist(void)
     const char* code;
     while ((code = FILESYSTEM_enumerateLanguageCodes(&handle)) != NULL)
     {
+        // Don't load languages with BIG fonts (for now)
+        if ((strcmp(code, "JA") == 0) || (strcmp(code, "KO") == 0) || (strcmp(code, "ZH") == 0) || (strcmp(code, "zh_TW") == 0)) continue;
+
         LangMeta meta;
         loadmeta(meta, code);
         if (meta.active)
